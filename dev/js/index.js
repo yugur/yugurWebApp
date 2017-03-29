d614b445ebab35360c95394178a3cebcef5063a9
Thomas Urwin <Thomas Urwin>
2017-03-29 11:10:15 +1100

    Init boilerplate project

---
+++ dev/js/index.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>Welcome To Yugur.io</h1>,
+    document.getElementById('root')
+);
