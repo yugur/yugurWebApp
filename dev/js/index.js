5dac0fa62e9baea959e85c34ca770dea9ca4e0ee
Thomas Urwin <Thomas Urwin>
2017-04-06 22:10:48 +1000

    minor color & css changes

--- dev/js/index.js
+++ dev/js/index.js
@@ -9,8 +9,8 @@ import App from './components/App';
 const store = createStore(allReducers);
 
 ReactDOM.render(
-	<Provider store={store}>
+  <Provider store={store}>
     <App />
-   </Provider>,
+  </Provider>,
    document.getElementById('root')
 );
