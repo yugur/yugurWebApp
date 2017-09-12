2b31c1014b629181cfa8521d7475c93389e4aac1
Nicholas Brown <nicholasbrown@users.noreply.github.com>
2017-09-12 14:26:26 +1000

    Remove passage analyser.
    - Remove passage analyser (see github.com/yugur/passage-analyser)

--- chinese-passage-analyzer/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	// Analyzer extension configureation 扩展配置
-	ext_dict: [/*'./dict/custom/mydict.dic', './dict/custom/single_word_low_freq.dic'*/],
-
-	// Users can set up a customised stopword dictionary in following uri 用户可以在这里配置自己的扩展停止词字典
-	ext_stopwords: [/*'./dict/custom/ext_stopword.dic'*/]
-};
