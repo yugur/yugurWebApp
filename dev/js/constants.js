bf02ff70b1152f516d8fd76bf8fa1e17bccbb71b
Thomas Urwin <thomas.urwin@hotmail.com>
2017-09-19 17:10:02 +1000

    fix typo

--- dev/js/constants.js
+++ dev/js/constants.js
@@ -1,5 +1,5 @@
 //Base URL for the hosted API
-export const BASE_API_URL = 'http://http://174.138.26.120:8080'
+export const BASE_API_URL = 'http://174.138.26.120:8080'
 
 //API Endpoints
 export const REGISTER_ENDPOINT = '/register'
