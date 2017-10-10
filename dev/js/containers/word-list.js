4c570a6ca8a27c86a2dc7c7d17501c91535ce759
Thomas Urwin <thomas.urwin@hotmail.com>
2017-10-10 21:46:21 +1100

    update response parsing

--- dev/js/containers/word-list.js
+++ dev/js/containers/word-list.js
@@ -7,6 +7,8 @@ class WordList extends Component {
 
 	createListItems() {
 		if (this.props.searchResults) {
+			let searchResults = JSON.parse(this.props.searchResults)
+			console.log(searchResults)
 			return this.props.searchResults.map((word) => {
 				return (
 					<li id="entry" key={word.id} onClick={() => this.props.selectWordEntry(word)}>
